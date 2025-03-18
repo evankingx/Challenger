9ddbed4572079e0f7ce4c0ff0e8a070feccba5d5
Evan King <evankingx@gmail.com>
2025-03-18 15:25:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class ReverseString
+    {
+        public static string Reversal(string str)
+        {
+            char[] strArray = str.ToCharArray();
+
+            string reversal = "";
+
+            for (int i = strArray.Length - 1; i > -1; i--)
+            {
+                reversal += strArray[i];
+            }
+            return reversal;
+
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(Reversal(Console.ReadLine()));
+        }
+    }
+}
