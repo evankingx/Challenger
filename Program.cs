755a2ef1d7cf8d231f0db4af65e00bff601c2b21
Evan King <evankingx@gmail.com>
2025-03-18 17:09:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ReverseString
+{
+    class PrimeTime
+    {
+        function PrimeTime(num)
+        {
+            for (let i = 2; i < num; i++)
+            {
+                if (num % i === 0)
+                {
+                    return false
+                }
+            }
+            return true;
+        }
+    }
+}
